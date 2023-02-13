4bec9bf254f7272bfa4e188ce9febda1c0cd8576
Syeda Iman Sarwar <69895631+imansarwar@users.noreply.github.com>
2023-02-13 13:52:49 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var dom = document.getElementById("chart-container");
+var myChart = echarts.init(dom, null, {
+  renderer: "canvas",
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+option = {
+  xAxis: {
+    type: "category",
+    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+  },
+  yAxis: {
+    type: "value"
+  },
+  series: [
+    {
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: "bar",
+      showBackground: true,
+      backgroundStyle: {
+        color: "rgba(180, 180, 180, 0.2)"
+      }
+    }
+  ]
+};
+
+if (option && typeof option === "object") {
+  myChart.setOption(option);
+}
+
+window.addEventListener("resize", myChart.resize);
